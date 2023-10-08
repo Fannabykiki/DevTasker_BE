97004697a530159494c543624d20835c9136d182
toanpv224 <toanpv224@gmail.com>
2023-10-08 22:08:49 +0700

    Upadte UpdateProfileValidator

---
+++ Capstone.API/Extentions/Validation/UserValidator/UpdateProfileValidator.cs
@@ -0,0 +1,28 @@
+﻿using Capstone.Common.DTOs.User;
+using FluentValidation;
+
+namespace Capstone.API.Extentions.Validation.UserValidator
+{
+	public class UpdateProfileValidator : AbstractValidator<UpdateProfileRequest>
+	{
+        public UpdateProfileValidator()
+        {
+            RuleFor(x => x.UserName)
+              .NotEmpty().WithMessage("User name is required");
+
+            RuleFor(x => x.PhoneNumber)
+              .Matches(@"^\d{10}$").WithMessage("Phone number must be 10 digits")
+              .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
+
+            RuleFor(x => x.Gender)
+              .IsInEnum().WithMessage("Invalid gender value");
+
+            RuleFor(x => x.Status)
+              .IsInEnum().WithMessage("Invalid status value");
+
+            RuleFor(x => x.Address)
+              .MaximumLength(100).WithMessage("Address must be less than 100 characters")
+              .When(x => !string.IsNullOrEmpty(x.Address));
+        }
+    }
+}
