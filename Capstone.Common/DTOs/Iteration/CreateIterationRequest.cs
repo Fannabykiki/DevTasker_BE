03bbe5977bb1d825ba365e910f3f8ad4e563c01c
Cuong Bui Quang <Cuongbui0110@gmail.com>
2023-10-28 07:33:20 +0700

    ipdate

--- Capstone.Common/DTOs/Iteration/CreateIterationRequest.cs
+++ Capstone.Common/DTOs/Iteration/CreateIterationRequest.cs
@@ -12,7 +12,7 @@ namespace Capstone.Common.DTOs.Iteration
         public string InterationName { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public Guid ProjectId { get; set; }
+        public Guid BoardId { get; set; }
         public InterationStatusEnum Status { get; set; }
     }
 }
