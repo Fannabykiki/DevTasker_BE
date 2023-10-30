d2b76c4ca75f03be06578b0c4511ff00fe967d37
Cuong Bui Quang <Cuongbui0110@gmail.com>
2023-10-30 08:44:51 +0700

    update

---
+++ Capstone.DataAccess/Entities/Status.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Capstone.DataAccess.Entities
+{
+    public class Status
+    {
+        [Key]
+        public Guid StatusId { get; set; }
+        public string Title { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Interation> Interations { get; set; }
+        public List<Board> Boards { get; set; }
+        public List<TicketHistory> TaskHistories { get; set; }
+    }
+}
