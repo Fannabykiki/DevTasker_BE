c982e9c1e3c1ca326787ec1e9e8166dd2dd5ebe0
toanpv224 <toanpv224@gmail.com>
2023-10-31 22:49:45 +0700

    Update Api manage Attachments

---
+++ Capstone.DataAccess/Repository/Interfaces/IStatusRepository.cs
@@ -0,0 +1,13 @@
+﻿using Capstone.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.DataAccess.Repository.Interfaces
+{
+    public interface IStatusRepository : IBaseRepository<Status>
+    {
+    }
+}
