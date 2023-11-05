272ad2b401dee35b2395cb5a43267ed1410922cb
toanpv224 <toanpv224@gmail.com>
2023-11-05 20:16:17 +0700

    Add service API comment

---
+++ Capstone.Common/DTOs/Comments/GetCommentResponse.cs
@@ -0,0 +1,21 @@
+﻿using Capstone.Common.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Common.DTOs.Comments
+{
+    public class GetCommentResponse
+    {
+        public Guid CommentId { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime? DeleteAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public Guid TaskId { get; set; }
+        public GetUserCommentResponse CreateByUser { get; set; } // 1 comment just create by 1 user
+    }
+}
---
+++ Capstone.Service/CommentService/CommentService.cs
@@ -0,0 +1,42 @@
+﻿using Capstone.Common.DTOs.Comments;
+using Capstone.DataAccess.Entities;
+using Capstone.DataAccess.Repository.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Service.CommentService
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ITicketCommentRepository _ticketCommentRepository;
+
+        public CommentService(ITicketCommentRepository ticketCommentRepository)
+        {
+            _ticketCommentRepository = ticketCommentRepository;
+        }
+
+        public Task<GetCommentResponse> CreateComment(CreateCommentRequest comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteComment(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<GetCommentResponse>> GetAllCommentByTaskID(Guid ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable> UpdateComment(Guid id, TicketComment updatedComment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
