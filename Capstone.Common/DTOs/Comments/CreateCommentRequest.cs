c8318fa358dcba1ca8fafe9bdf7618cdfd3a8f10
toanpv224 <toanpv224@gmail.com>
2023-11-06 15:44:33 +0700

    add comment validatator

--- Capstone.Common/DTOs/Comments/CreateCommentRequest.cs
+++ Capstone.Common/DTOs/Comments/CreateCommentRequest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Capstone.Common.DTOs.Comments
+﻿namespace Capstone.Common.DTOs.Comments
 {
     public class CreateCommentRequest
     {
