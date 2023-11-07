54be5885b4d42285823ce2e0d13ff63d04cbb7f0
FPTU | Fannaby <namplhe153292@fpt.edu.vn>
2023-11-07 23:04:59 +0700

    fix bug

--- Capstone.Common/DTOs/Project/GetAllProjectResponse.cs
+++ Capstone.Common/DTOs/Project/GetAllProjectResponse.cs
@@ -1,9 +1,22 @@
 ﻿using Capstone.Common.DTOs.Paging;
+using Capstone.Common.DTOs.User;
 
 namespace Capstone.Common.DTOs.Project
 {
 	public class GetAllProjectResponse
     {
-        public PagedResponse<GetAllProjectViewModel> pagination { get; set; }
+		public Guid ProjectId { get; set; }
+		public string ProjectName { get; set; }
+		public string Description { get; set; }
+		public string ProjectStatus { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public UserResponse Manager { get; set; }
+		public List<UserResponse>? Member { get; set; }
+		public DateTime CreateAt { get; set; }
+		public DateTime? DeleteAt { get; set; }
+		public DateTime? ExpireAt { get; set; }
+		public bool PrivacyStatus { get; set; } // false: Private , true: Pu
+		public PagedResponse<GetAllProjectViewModel> pagination { get; set; }
     }
 }
--- Capstone.Service/ProjectService/IProjectService.cs
+++ Capstone.Service/ProjectService/IProjectService.cs
@@ -7,7 +7,7 @@ namespace Capstone.Service.ProjectService;
 
 public interface IProjectService
 {
-    Task<IEnumerable<GetAllProjectResponse>> GetProjectsAdmin();
+	Task<PagedResponse<GetAllProjectResponse>> GetProjectsAdmin(int limit, int page);
 	Task<IEnumerable<GetUserProjectAnalyzeResponse>> GetUserProjectAnalyze(Guid userId);
     Task<ProjectAnalyzeRespone> ProjectAnalyzeAdmin();
     Task<CreateProjectRespone> CreateProject(CreateProjectRequest createProjectRequest, Guid userId);
