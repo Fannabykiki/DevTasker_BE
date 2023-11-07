5011aeb2f478084361f5433ae9a687de56b15be4
Cuong Bui Quang <Cuongbui0110@gmail.com>
2023-11-07 16:26:04 +0700

    add comment service unit test

--- NUnitTest.DevTasker/Service/CommentServiceTest.cs
+++ NUnitTest.DevTasker/Service/CommentServiceTest.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Capstone.DataAccess.Repository.Interfaces;
+using Capstone.Service.TicketCommentService;
+using Moq;
+using NUnit.Framework;
 
 namespace NUnitTest.DevTasker.Service
 {
+    [TestFixture]
     public class CommentServiceTest
     {
+        private ITicketCommentService _iticketService; 
+        private Mock <ITicketCommentRepository> _ticketCommentRepository;
+        private Mock <ITicketRepository> _ticketRepository;
+        private Mock <IUserRepository> _userRepository;
+        private Mock<IMapper> _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ticketCommentRepository = new Mock<ITicketCommentRepository>();
+            _ticketRepository = new Mock<ITicketRepository>();
+            _mapper = new Mock<IMapper>();
+            _userRepository = new Mock<IUserRepository>();
+
+            _iticketService = new TicketCommentService
+                (
+                _ticketCommentRepository.Object,
+                _mapper.Object,
+                _ticketRepository.Object,
+                _userRepository.Object
+                );
+        }
+
+      
+
+
     }
 }
