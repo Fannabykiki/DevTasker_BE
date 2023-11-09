afce6b5eda8abfb690aa9a8e6091bf3ce7490aad
FPTU | Fannaby <namplhe153292@fpt.edu.vn>
2023-11-09 15:42:11 +0700

    add accept invitaion project member

--- Capstone.Service/ProjectMemberService/IProjectMemberService.cs
+++ Capstone.Service/ProjectMemberService/IProjectMemberService.cs
@@ -1,10 +1,12 @@
 ﻿using Capstone.Common.DTOs.Project;
 using Capstone.Common.DTOs.ProjectMember;
+using Capstone.DataAccess.Entities;
 
 namespace Capstone.Service.ProjectMemberService
 {
 	public interface IProjectMemberService
 	{
+		Task<bool> AcceptInvitation(Guid userId,Guid projectId);
 		Task<AddNewProjectMemberResponse> AddNewProjectMember(InviteUserRequest inviteUserRequest);
 	}
 }
