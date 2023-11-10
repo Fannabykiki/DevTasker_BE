fa191c81887a0544437c221d3dca604adcf39eee
toanpv224 <toanpv224@gmail.com>
2023-11-10 14:05:56 +0700

    fix test project erorr

--- Capstone.Service/PermissionSchemaService/PermissionSchemaService.cs
+++ Capstone.Service/PermissionSchemaService/PermissionSchemaService.cs
@@ -29,6 +29,13 @@ namespace Capstone.Service.PermissionSchemaService
         private readonly ILoggerManager _logger;
         private readonly IMapper _mapper;
 
+        public object Value { get; }
+        public IPermissionSchemaRepository Object1 { get; }
+        public ISchemaRepository Object2 { get; }
+        public IRoleRepository Object3 { get; }
+        public IPermissionRepository Object4 { get; }
+        public IMapper Object5 { get; }
+
         public PermissionSchemaService(ILoggerManager logger, IPermissionSchemaRepository permissionSchemaRepository, ISchemaRepository schemaRepository, IRoleRepository roleRepository, IPermissionRepository permissionRepository, IMapper mapper,IProjectRepository projectRepository)
         {
             _permissionSchemaRepository = permissionSchemaRepository;
@@ -39,6 +46,17 @@ namespace Capstone.Service.PermissionSchemaService
             _mapper = mapper;
             _projectRepository = projectRepository;
         }
+
+        public PermissionSchemaService(object value, IPermissionSchemaRepository object1, ISchemaRepository object2, IRoleRepository object3, IPermissionRepository object4, IMapper object5)
+        {
+            Value = value;
+            Object1 = object1;
+            Object2 = object2;
+            Object3 = object3;
+            Object4 = object4;
+            Object5 = object5;
+        }
+
         public async Task<List<GetSchemaResponse>> GetAllSchema()
         {
             var schemas = await _schemaRepository.GetAllWithOdata(x => true, null);
