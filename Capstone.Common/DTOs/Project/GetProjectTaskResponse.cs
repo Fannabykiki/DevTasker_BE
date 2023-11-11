eba0b72997813d12ec36c07064e3fce0ec624233
toanpv224 <toanpv224@gmail.com>
2023-11-11 21:58:21 +0700

    add DTO Tasks

---
+++ Capstone.Common/DTOs/Project/GetProjectTaskResponse.cs
@@ -0,0 +1,25 @@
+﻿
+using Capstone.Common.DTOs.Iteration;
+using Capstone.Common.DTOs.TaskType;
+using Capstone.Common.DTOs.User;
+
+namespace Capstone.Common.DTOs.Project
+{
+    public class GetProjectTasksResponse
+    {
+        public Guid TaskId { get; set; }
+        public string Title { get; set; }
+        public string? Decription { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime? DeleteAt { get; set; }
+        public UserResponse AssignTo { get; set; }
+        public UserResponse CreateBy { get; set; }
+        public string TaskType { get; set; } 
+        public Guid? PrevId { get; set; }
+        public string TaskStatus { get; set; }
+        public string Priority { get; set; }
+        public string Interation { get; set; }
+    }
+}
