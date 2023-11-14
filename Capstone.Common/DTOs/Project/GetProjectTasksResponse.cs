41ec64274131c9f151a9608a80350cb18afee2fb
FPTU | Fannaby <namplhe153292@fpt.edu.vn>
2023-11-14 17:55:47 +0700

    change description

--- Capstone.Common/DTOs/Project/GetProjectTasksResponse.cs
+++ Capstone.Common/DTOs/Project/GetProjectTasksResponse.cs
@@ -9,7 +9,7 @@ namespace Capstone.Common.DTOs.Project
     {
         public Guid TaskId { get; set; }
         public string Title { get; set; }
-        public string? Decription { get; set; }
+        public string? Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime DueDate { get; set; }
         public DateTime CreateTime { get; set; }
