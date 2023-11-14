f3ab0c235393508e832e96bc3cdb68fc917a1334
FPTU | Fannaby <namplhe153292@fpt.edu.vn>
2023-11-14 21:01:47 +0700

    add total task created and complete

--- Capstone.DataAccess/Repository/Interfaces/ITaskRepository.cs
+++ Capstone.DataAccess/Repository/Interfaces/ITaskRepository.cs
@@ -6,6 +6,7 @@ namespace Capstone.DataAccess.Repository.Interfaces
     public interface ITaskRepository : IBaseRepository<Entities.Task>
     {
         Task<List<TaskViewModel>> GetAllTask(Guid projectId);
+        Task<List<TaskViewModel>> GetAllTaskCompleted(Guid projectId,Guid statusId);
         Task<List<TaskViewModel>> GetAllTaskDelete(Guid projectId);
     }
 }
