179774d12b958d74226726b0933855cf9573f116
toanpv224 <toanpv224@gmail.com>
2023-11-16 22:13:41 +0700

    update relationships

---
+++ Capstone.Common/DTOs/Project/GetProjectReportRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Common.DTOs.Project
+{
+    public class GetProjectReportRequest
+    {
+        public ReportRecord reportRecord { get; set; }
+        public List<ReportRecord> reportRecordByWeerk { get; set; }
+
+    }
+    public class ReportRecord 
+    {
+        public int TotalTask { get; set; }
+        public DateTime? DateTime { get; set; }
+        public List<ReportStatus> reportStatuses { get; set; }
+    }
+    public class ReportStatus
+    {
+        public Guid BoardStatusId { get; set; }
+        public string Title { get; set; }
+        public int? NumberTask { get; set; }
+        public int? Percent { get; set; }
+
+    }
+}
