26e85d815bb9cee71aedde5c2f28ba4c52afffe1
toanpv224 <toanpv224@gmail.com>
2023-11-18 16:20:30 +0700

    fix error

--- Capstone.Service/ProjectService/ProjectService.cs
+++ Capstone.Service/ProjectService/ProjectService.cs
@@ -757,13 +757,11 @@ public class ProjectService : IProjectService
 		}
 	}
 
-		return await _projectRepository.GetProjectReport(projectId);
-    }
-
     public async Task<List<GetProjectCalendarResponse>> GetProjectCalender(Guid projectId)
     {
         return await _projectRepository.GetProjectCalender(projectId);
     }
+
 	public async Task<InvitationResponse> CheckInvation(Guid invationId)
 	{
 		var result = await _invitationRepository.GetInvitation(invationId);
