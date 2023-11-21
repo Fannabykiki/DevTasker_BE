559e893af2940e694f7e4e7a42c258b2d556d75d
Cuong <cuongbui0110@gmail.com>
2023-11-21 22:07:03 +0700

    Add  notif hub and email job
    ÁDASDASD

---
+++ Capstone.Common/DTOs/Notification/NotificationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Common.DTOs.Notification
+{
+    public class NotificationViewModel
+    {
+        public Guid NotificationId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string? TargetUrl { get; set; }
+        public bool IsRead { get; set; } //false : Not yet , True : Already
+        public Guid RecerverId { get; set; }
+    }
+}
---
+++ Capstone.Service/Hubs/PresenceTracker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Service.Hubs
+{
+    public class PresenceTracker
+    {
+        private static readonly Dictionary<string, List<string>> OnlineUsers =
+            new Dictionary<string, List<string>>();
+
+        public Task<bool> UserConnected(string userid, string connectionId)
+        {
+            bool isOnline = false;
+            lock (OnlineUsers)
+            {
+                if (OnlineUsers.ContainsKey(userid))
+                {
+                    OnlineUsers[userid].Add(connectionId);
+                }
+                else
+                {
+                    OnlineUsers.Add(userid, new List<string> { connectionId });
+                    isOnline = true;
+                }
+            }
+
+            return Task.FromResult(isOnline);
+        }
+
+        public Task<bool> UserDisconnected(string userid, string connectionId)
+        {
+            bool isOffline = false;
+            lock (OnlineUsers)
+            {
+                if (!OnlineUsers.ContainsKey(userid)) return Task.FromResult(isOffline);
+
+                OnlineUsers[userid].Remove(connectionId);
+                if (OnlineUsers[userid].Count == 0)
+                {
+                    OnlineUsers.Remove(userid);
+                    isOffline = true;
+                }
+            }
+
+            return Task.FromResult(isOffline);
+        }
+
+        public Task<string[]> GetOnlineUsers()
+        {
+            string[] onlineUsers;
+            lock (OnlineUsers)
+            {
+                onlineUsers = OnlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+            }
+
+            return Task.FromResult(onlineUsers);
+        }
+
+        public Task<List<string>> GetConnectionsForUser(string userId)
+        {
+            List<string> connectionIds;
+            lock (OnlineUsers)
+            {
+                connectionIds = OnlineUsers.GetValueOrDefault(userId   );
+            }
+
+            return Task.FromResult(connectionIds);
+        }
+    }
+}
