beb81db1c20d9924459c85fa205b264c314c9b85
Cuong <cuongbui0110@gmail.com>
2023-11-28 18:10:25 +0700

    add hangfire job

---
+++ Capstone.API/Controllers/Testing/TestingJobController.cs
@@ -0,0 +1,22 @@
+﻿using Capstone.API.Jobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.API.Controllers.Testing
+{
+    [Route("api/job-testing")]
+    [ApiController]
+    public class TestingJobController : ControllerBase
+    {
+        private readonly IEmailJob _emailJob;
+        public TestingJobController(IEmailJob emailJob)
+        {
+            _emailJob = emailJob;
+        }
+        [HttpGet("job-run")]
+        public async Task<IActionResult> GetJobRun()
+        {
+            await _emailJob.RunJob();
+            return Ok();
+        }
+    }
+}
